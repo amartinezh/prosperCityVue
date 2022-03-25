00baf1547ae43a50d9485a3dfebb747838d7eeb9
Andrés Mauricio Martínez Hincapié - Ceiba Software <andres.hincapie@ceiba.com.co>
2022-03-25 18:09:46 -0500

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo_p.jpg" style="width:60%">
+    <HelloWorld msg="Bienvenidos a una gran esperiencia"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+  },
+});
+</script>

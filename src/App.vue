81ab063670f5ccb8315a2da187044402fd46f157
Hikotsuoficial <derisccc@gmail.com>
2022-04-07 13:46:33 -0500

    Comit equipo2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <nav>
-    <router-link to="/">--Inicio--</router-link> |
-    <router-link to="/about">-- Acerca de ... --</router-link> |
-    <router-link to="/tarea">-- Tarea Abril 1 --</router-link> |
+    <router-link to="/">Inicio</router-link>
+    <router-link to="/about">Acerca de</router-link>
+    <router-link to="/tarea">Tarea Abril 1</router-link>
     <router-link to="/grupoRojo">Grupo Rojo</router-link>
+    <router-link to="/Azul">Grupo Azul</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
+*{
+  box-sizing: border-box;
+  margin:0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,12 +24,17 @@
 }
 
 nav {
-  padding: 30px;
+  background-color: #2c3e50;
+  display: flex;
+  justify-content: center;
+  padding: 15px 30px;
 }
 
 nav a {
+  padding: 25px;
   font-weight: bold;
-  color: #2c3e50;
+  color: #fff;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
---
+++ src/views/GrupoAzul.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="azul">
+        <h1>HOLA SOMOS EL GRUPO AZUL</h1>
+        <h4>Los integrantes somos:</h4>
+        <ul>
+            <li>Emanuel</li>
+            <li>Dayhana</li>
+            <li>Sebastian</li>
+            <li>Juan Felipe</li>
+            <li>Andy</li>
+            <li>jhon Henry</li>
+            <li>Andrea</li>
+            <li>Erisc</li>
+        </ul>
+        <p><i>Aqui abajo pondremos el componente:</i></p>
+    </div>
+</template>
+
+<style scoped>
+.azul{
+    margin: 10px auto;
+}
+ul{
+    margin: 15px 0;
+}
+</style>

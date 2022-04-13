730c5588e0fa5cd8101d36d8d4067b61f15f636a
AlexisQuiceno <alexisquicenorodriguez@gmail.com>
2022-04-13 12:53:25 -0500

    Componente de tiempo agregado por Alexis

--- src/components/comPalexis.vue
+++ src/components/comPalexis.vue
@@ -1,20 +1,50 @@
-<template>
-    <div>
-        <h1>{{name}}</h1>
-    </div>
-</template>
-
 <script>
-import { defineComponent } from 'vue';
-
-export default defineComponent({
+export default {
   data() {
     return {
-      name: 'Aqui aparecera Bootstrap',
+      duration: 15 * 1000,
+      elapsed: 0,
     };
   },
-});
+  created() {
+    let lastTime = performance.now();
+    const update = () => {
+      const time = performance.now();
+      this.elapsed += Math.min(time - lastTime, this.duration - this.elapsed);
+      lastTime = time;
+      this.handle = requestAnimationFrame(update);
+    };
+    update();
+  },
+  unmounted() {
+    cancelAnimationFrame(this.handle);
+  },
+};
 </script>
-<style lang="stylus" scoped>
 
+<template>
+<form action="">
+  <progress :value="elapsed / duration"></progress>
+</form>
+  <div>{{ (elapsed / 1000).toFixed(1) }}s</div>
+
+  <div>
+    <label for="name">Duration:
+    <input type="range" v-model="duration" min="1" max="30000">
+    </label>
+    {{ (duration / 1000).toFixed(1) }}s
+  </div>
+
+  <button @click="elapsed = 0">Reset</button>
+</template>
+
+<style>
+.elapsed-container {
+  width: 300px;
+}
+
+.elapsed-bar {
+  background-color: red;
+  height: 10px;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { createApp } from 'vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App.vue';
 import router from './router';
 

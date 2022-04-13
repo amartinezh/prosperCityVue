ef953903741cf872a37f67c9142299d44e83c397
AlexisQuiceno <alexisquicenorodriguez@gmail.com>
2022-04-13 13:47:17 -0500

    correccion de importacion

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { createApp } from 'vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
 import App from './App.vue';
 import router from './router';
 

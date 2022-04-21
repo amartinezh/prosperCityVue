3e90a355b6eb9698f711a15e958e1949c4e44c42
Dayana Betancur <dahianab01@gmail.com>
2022-04-21 17:14:27 -0500

    Correcto

--- src/components/LinkTreeRojo.vue
+++ src/components/LinkTreeRojo.vue
@@ -1,8 +1,21 @@
 <template>
+<div class="linktree-container">
     <aside class="aside">
-        aside
+      aside
     </aside>
-    <previewComponent/>
+    <main class="main">
+      <div class="main-container">
+        <div class="profile-container">
+          <div class="profile">Ver perfil</div>
+          <div class="edit-profile">Editar perfil</div>
+        </div>
+        <previewComponent/>
+        <div class="add-link">Add link</div>
+        <div class="link-list">Link List</div>
+      </div>
+    </main>
+  </div>
+  <previewComponent/>
 </template>
 
 <script lang="ts">
@@ -18,5 +31,52 @@ export default defineComponent({
 });
 </script>
 <style>
-
+    root {
+       --blue: blue;
+       --red: red;
+    }
+    .linktree-container {
+      display: grid;
+      font-family: Arial, Helvetica, sans-serif;
+      grid-template-rows: 1fr 60px;
+      grid-template-columns: 1fr;
+      height: 100vh;
+      margin: 0;
+    }
+    .aside{
+      background: #ffd2d2;
+      grid-area: 2 / 1 / 3 / 2;
+    }
+    .main {
+      background: #F9F9F9;
+    }
+    .main-container {
+      max-width: 740px;
+      margin: 0 auto;
+    }
+    .profile-container {
+      background: rgb(205, 255, 205);
+    }
+    .preview-container {
+      background: rgb(193, 214, 255);
+    }
+    .add-link {
+      background: rgb(255, 255, 186);
+    }
+    .link-list {
+      background: rgb(255, 196, 255);
+    }
+    @media (min-width: 768px) {
+      .linktree-container {
+        grid-template-columns: 180px 1fr;
+        grid-template-rows: 1fr;
+      }
+      .main {
+        height: 100vh;
+      }
+      .aside {
+        grid-area: initial;
+        height: 100vh;
+      }
+    }
 </style>

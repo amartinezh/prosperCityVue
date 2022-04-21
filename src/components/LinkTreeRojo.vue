9559a9cdeab7b051881a4325cca380cd1608f5ee
Dayana Betancur <dahianab01@gmail.com>
2022-04-21 16:36:41 -0500

    Base link tree

---
+++ src/components/LinkTreeRojo.vue
@@ -0,0 +1,22 @@
+<template>
+    <aside class="aside">
+        aside
+    </aside>
+    <previewComponent/>
+</template>
+
+<script lang="ts">
+
+import previewComponent from '@/components/previewComponent.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LinkTreeRojo',
+  components: {
+    previewComponent,
+  },
+});
+</script>
+<style>
+
+</style>
--- src/views/calculadoraView.vue
+++ src/views/calculadoraView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     Vista dedicada a mostar un componente
     <sumarComponent/>
+    <LinkTreeRojo/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import LinkTreeRojo from '@/components/LinkTreeRojo.vue';
 import sumarComponent from '@/components/sumarComponent.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'calculadoraView',
   components: {
     sumarComponent,
+    LinkTreeRojo,
   },
 });
 </script>

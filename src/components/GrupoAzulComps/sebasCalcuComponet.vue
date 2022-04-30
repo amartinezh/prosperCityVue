948720f0eced7732a3ba230531e0366511697dd3
ElSebastianLopez <sebaslopezhenao@gmail.com>
2022-04-30 01:11:38 -0500

    creo un componente

---
+++ src/components/GrupoAzulComps/sebasCalcuComponet.vue
@@ -0,0 +1,18 @@
+<template>
+<div></div>
+</template>
+<
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
+

aa10ea49f8c6a75c1522aceb5b69efabd1af28bf
klvin23 <klvin.g23@gmail.com>
2022-08-05 17:15:49 -0500

    creacion de componente Friends en archivo components

---
+++ src/components/Friends.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="new-component">
+    <h1>{{ friends }}</h1>
+    <p> Nuevo componente </p>
+</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent ({
+    name: 'friends',
+    props: {
+        friends: String,
+    },
+});
+</script>

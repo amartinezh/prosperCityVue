81e239d27bc94bf9dabb1036f41a20cea6e5e2d9
jj <jj@jj>
2022-08-07 00:14:01 -0500

    reunion grupal donde se creo los componentes y vistas para division de trabajo

---
+++ src/views/MultimediaView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <multimedia titulo_tarea="Titulo de la tarea" msg="Comida"/>
+    <fotos/>
+    <videos/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import multimediaFotos from '@/components/MultimediaFotos.vue'; // @ is an alias to /src
+import multimediaVideos from '@/components/MultimediaVideo.vue';
+
+export default defineComponent({
+  name: 'MultimediaView',
+  components: {
+    fotos: multimediaFotos,
+    videos: multimediaVideos,
+
+  },
+});
+
+</script>

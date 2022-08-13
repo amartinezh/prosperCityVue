1e1e6b306251610123252dcf4db479025d40ae4e
AydaMontoya <aidamontoya96@gmail.com>
2022-08-13 00:02:54 -0500

    Reunion Grupal, creamos componentes

---
+++ src/components/BitFlow.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <br />
+    <hr />
+    <h1>{{ titulo }}</h1>
+    <h1>{{ msg }}</h1>
+    <br/>
+    <h2>
+      <p style="font-size:30px">&#128540;</p>
+    </h2>
+    <p>Ayda Montoya Henao<br/>
+     Jose Luis Giraldo Morales <br/>
+     Viviana Galvis <br/>
+     Kendy Correa Torralba <br/>
+     Valentina Quiroga <br/>
+     Anyi Hernandez <br/>
+     Juan David Ramirez <br/>
+     Yeny Constanza <br/>
+     </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ComponenteEjemploComponent',
+  props: {
+    msg: String,
+    titulo: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/ComponenteEjemplo.vue
+++ src/views/ComponenteEjemplo.vue
@@ -4,17 +4,20 @@
     <ComponenteEjemploComponent titulo="Titulo del componente" msg="Bienvenido"/>
     <ComponenteEjemploComponent titulo="Otro Component" msg="Bienvenido"/>
     <ComponenteEjemploComponent titulo="El equipo de los MAS" msg="Bienvenido"/>
+    <BitFlow titulo="BitFlow" msg="Integrantes de nuestro maravilloso equipo"/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import ComponenteEjemploComponent from '@/components/Componente_ejemplo.vue'; // @ is an alias to /src
+import BitFlow from '@/components/BitFlow.vue';
 
 export default defineComponent({
   name: 'ComponenteView',
   components: {
     ComponenteEjemploComponent,
+    BitFlow,
   },
 });
 </script>

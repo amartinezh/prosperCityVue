4fd8d4e2696107bda126dc814c5e80142d83f1d3
Anlly Carolina Monsalve Lalinde <angielalinde77@gmail.com>
2022-08-13 21:30:59 -0500

    Agrego componente Anlly

---
+++ src/components/ComponentAnlly.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  <h2>Componente Anlly</h2>
+</div>
+</template>
+<script lang="ts">
+</script>
+<style>
+h2{
+    color:red;
+}
+</style>
--- src/views/CaciqueCandela.vue
+++ src/views/CaciqueCandela.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
     <ComponentCindy/>
+    <ComponentAnlly/>
     </div>
 </template>
 <script lang="ts">
 import ComponentCindy from '@/components/ComponentCindy.vue';
+import ComponentAnlly from '@/components/ComponentAnlly.vue';
 
 export default {
   components: {
     ComponentCindy,
+    ComponentAnlly,
   },
 };
 </script>

aa5986965c2d8d3f2dae4318a8aeefdab5c91bf0
Mauricio Muñoz <maomunoz2418>
2022-08-15 12:09:07 -0500

    agregue estructura basica al componente de Cindy

---
+++ src/components/ComponentCindy.vue
@@ -0,0 +1,15 @@
+<template>
+ <div>
+  <h2>componente Cindy</h2>
+    <p class="texto">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+      consectetur reiciendis architecto dolorum est officia quos nihil dicta
+      quaerat, facere ullam, porro labore nesciunt. Dolorum natus dolor culpa
+      repellendus iure?
+    </p>
+ </div>
+</template>
+<script lang="ts">
+</script>
+<style>
+</style>
--- src/components/ComponentMauricio.vue
+++ src/components/ComponentMauricio.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
     <h2>componente de Maurucio</h2>
-    <p>
+    <p class="texto">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
       consectetur reiciendis architecto dolorum est officia quos nihil dicta
       quaerat, facere ullam, porro labore nesciunt. Dolorum natus dolor culpa
       repellendus iure?
     </p>
+
   </div>
 </template>
 <script lang="ts">
@@ -14,5 +15,10 @@
 <style scoped>
 h2 {
   color: green;
+}
+.texto{
+    text-align: center;
+    color:blue;
+    font-size: 40px;
 }
 </style>

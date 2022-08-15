f4cfb06f2cfc8e0c9da34a10a9f3966dbba0c601
Ricardo Montoya <ricardomontoyain@gmail.com>
2022-08-15 17:18:26 -0500

    cacique

---
+++ src/components/ComponentRicardo.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  <h2>Cacique Candela</h2>
+  <h1>Hypopyrrhus pyrohypogaster</h1>
+  <p>El pajaro ese</p>
+</div>
+</template>
+<script lang="ts">
+</script>
+<style>
+
+</style>
--- src/views/CaciqueCandela.vue
+++ src/views/CaciqueCandela.vue
@@ -3,19 +3,22 @@
     <ComponentCindy/>
     <ComponentAnlly/>
     <ComponentMauricio/>
+    <ComponentRicardo/>
     </div>
 </template>
 <script lang="ts">
 import ComponentCindy from '@/components/ComponentCindy.vue';
 import ComponentAnlly from '@/components/ComponentAnlly.vue';
 import ComponentMauricio from '@/components/ComponentMauricio.vue';
+import ComponentRicardo from '@/components/ComponentRicardo.vue';
 
 export default {
   components: {
     ComponentCindy,
     ComponentAnlly,
     ComponentMauricio,
-  },
+    ComponentRicardo,
+},
 };
 </script>
 <style scoped>

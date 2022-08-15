89621f54a733067e3d4eb7c4e15d19e5d56d885c
Mauricio Muñoz <maomunoz2418>
2022-08-15 11:45:41 -0500

    agrego componente  mauricio

---
+++ src/components/ComponentMauricio.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>componente de Maurucio</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+      consectetur reiciendis architecto dolorum est officia quos nihil dicta
+      quaerat, facere ullam, porro labore nesciunt. Dolorum natus dolor culpa
+      repellendus iure?
+    </p>
+  </div>
+</template>
+<script lang="ts">
+</script>
+<style scoped>
+h2 {
+  color: green;
+}
+</style>
--- src/views/CaciqueCandela.vue
+++ src/views/CaciqueCandela.vue
@@ -2,16 +2,19 @@
     <div>
     <ComponentCindy/>
     <ComponentAnlly/>
+    <ComponentMauricio/>
     </div>
 </template>
 <script lang="ts">
 import ComponentCindy from '@/components/ComponentCindy.vue';
 import ComponentAnlly from '@/components/ComponentAnlly.vue';
+import ComponentMauricio from '@/components/ComponentMauricio.vue';
 
 export default {
   components: {
     ComponentCindy,
     ComponentAnlly,
+    ComponentMauricio,
   },
 };
 </script>

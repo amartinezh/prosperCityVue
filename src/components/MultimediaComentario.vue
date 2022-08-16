f27c4fab3b95de73e87c0879ac4b64551570e2f4
jj <jj@jj>
2022-08-16 01:22:43 -0500

    funcionando comentarios

--- src/components/MultimediaComentario.vue
+++ src/components/MultimediaComentario.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>hola mundo</h1>
+<label for="name"><input @change="prueba" type="text" /></label>
+<button @click="enviar">comentar</button>
+<h3 v-for="(comentario, llave) in comentarios" :key = "llave" >{{comentario}} </h3>
+
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      comentarios: [],
+    };
+  },
+  methods: {
+    prueba(e) {
+      console.log(e.target.value);
+      this.comentarios.push(e.target.value);
+    },
+  },
+};
+</script>

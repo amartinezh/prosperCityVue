35c3a7f5e6c5483ff3535769541d8312d68bb2da
Jose Luis <jj@jj>
2022-08-17 13:24:05 -0500

    se corrigieron los comentarios y se hizo el merge con la rama bitflow-videos

--- src/components/ComponentesMultimedia/MultimediaComentario.vue
+++ src/components/ComponentesMultimedia/MultimediaComentario.vue
@@ -2,13 +2,14 @@
 <br>
 <h1>Que opinas de nuestra galería? Déjanos tu comentario:</h1>
 <br>
-<ul class="list-grouplist-group-flush">
-  <li class="list-group-item" v-for="(c) in comentarios" :key = "c">{{c}} </li>
-</ul>
+
 <label for="name">
   <input @change="prueba" class="form-control" placeholder="Ingresa tu comentario" type="text" />
 </label><br><br>
 <button @click="enviar" class="btn btn-primary btn-sm">Comentar</button><br>
+<ul class="list-grouplist-group-flush">
+  <li class="list-group-item" v-for="(c) in comentarios" :key = "c">{{c}} </li>
+</ul>
 
 </template>
 

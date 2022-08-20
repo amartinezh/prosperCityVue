1782644d9548774df6b0d1913ca7bd9d7c5cca02
Jose Luis <jj@jj>
2022-08-20 10:20:15 -0500

    corregir espacios

--- src/components/ComponentesMultimedia/MultimediaFotos.vue
+++ src/components/ComponentesMultimedia/MultimediaFotos.vue
@@ -1,5 +1,5 @@
 <template>
-    <h2><b>Fotografías</b></h2>
+    <h2>Fotografías</h2>
     <br>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <div class="imagen-container">
@@ -9,20 +9,55 @@
             <img v-bind:src= "i.imagen" alt='no dio' v-bind:title= "i.descripcion">
         </div>
     </div>
+    <div class="imagen-container" v-for="i in imagenes" :key = i>
+      <div class="imagen-item imagen-8">
+          <h3 class="subtitle" >prueba</h3>
+          <hr>
+          <a href="#!"  data-bs-toggle="modal" v-bind:data-bs-target=i.modal>
+          <img v-bind:src= i.imagen alt='no dio' title= "prueba">
+        </a>
+      </div>
+          <!-- modal galery -->
+    <div tabindex="-1" v-bind:aria-labelledby=i.modal_id aria-hidden="true"
+     class="modal fade" v-bind:id=i.modal_id>
+      <div class="modal-dialog modal-lg modal-dialog-centered">
+          <div class="modal-content">
+              <img v-bind:src=i.imagen alt="">
+          </div>
+      </div>
+  </div>
+  <!-- find modal galery -->
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      imagenes: [{ imagen: 'https://i.postimg.cc/R07FYSXh/AI-02.jpg', descripcion: 'Calidad de vida' },
-        { imagen: 'https://i.postimg.cc/Dz59VzCw/AI-4.jpg', descripcion: 'Autoconciencia' },
-        { imagen: 'https://i.postimg.cc/gjCbdR0W/AI-12.jpg', descripcion: 'Realidad virtual' },
-        { imagen: 'https://i.postimg.cc/8C0VJ5Ln/AI-6.jpg', descripcion: 'Agentes inteligentes' },
-        { imagen: 'https://i.postimg.cc/gjQmJQN1/AI-16.jpg', descripcion: 'Globalizacion inteligente' },
-        { imagen: 'https://i.postimg.cc/HLLmwbVp/AI-20.jpg', descripcion: 'Intelecto artificial' },
-        { imagen: 'https://i.postimg.cc/hv0xsHD6/AI-1.jpg ', descripcion: 'IA vs humanos' },
-        { imagen: 'https://i.postimg.cc/J0CvZB0K/AI-9.jpg', descripcion: 'Metaverso' }],
+      imagenes: [{
+        imagen: 'https://i.postimg.cc/R07FYSXh/AI-02.jpg', descripcion: 'Calidad de vida', modal: '#item1', modal_id: 'item1',
+      },
+      {
+        imagen: 'https://i.postimg.cc/Dz59VzCw/AI-4.jpg', descripcion: 'Autoconciencia', modal: '#item2', modal_id: 'item2',
+      },
+      {
+        imagen: 'https://i.postimg.cc/gjCbdR0W/AI-12.jpg', descripcion: 'Realidad virtual', modal: '#item3', modal_id: 'item3',
+      },
+      {
+        imagen: 'https://i.postimg.cc/8C0VJ5Ln/AI-6.jpg', descripcion: 'Agentes inteligentes', modal: '#item4', modal_id: 'item4',
+      },
+      {
+        imagen: 'https://i.postimg.cc/gjQmJQN1/AI-16.jpg', descripcion: 'Globalizacion inteligente', modal: '#item5', modal_id: 'item5',
+      },
+      {
+        imagen: 'https://i.postimg.cc/HLLmwbVp/AI-20.jpg', descripcion: 'Intelecto artificial', modal: '#item6', modal_id: 'item6',
+      },
+      {
+        imagen: 'https://i.postimg.cc/hv0xsHD6/AI-1.jpg ', descripcion: 'IA vs humanos', modal: '#item7', modal_id: 'item7',
+      },
+      {
+        imagen: 'https://i.postimg.cc/J0CvZB0K/AI-9.jpg', descripcion: 'Metaverso', modal: '#item8', modal_id: 'item8',
+      }],
       imagen: '',
     };
   },

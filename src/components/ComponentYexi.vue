6aa121a292f92a7ca063cc9ef56c54c14fc0c15f
yexialexandra <yexialexa90@gmail.comgit config -- user.name yexialexandragit config --global user.email yexialexa90@gmail.com>
2022-08-20 20:49:34 -0500

    informacion colibri por yexi

--- src/components/ComponentYexi.vue
+++ src/components/ComponentYexi.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
-    <h2>componente de Yexi</h2>
-    <div>
-        <img alt="Vue logo" src="@/assets/colibrilazulita.jpg">
+    <p class="titulocomponente">Colibrí la Azulita</p>
+    <div class="contenedorimagen">
+      <img alt="Vue logo" src="@/assets/colibrilazulita.jpg" />
     </div>
+    <p class="infocolibrilaazulita">
+      Colibrí Lazulita / Campyloptetrus falcatus.Muy territorial, difícil de observar en Caldas, se
+      alimenta principalmente de nectar, complementa su dieta con pequeños insectos. en Pensilvania
+      se observa en el Centro vacacional el Bosque.
+    </p>
   </div>
-
 </template>
-<script lang="ts">
-</script>
+<script lang="ts"></script>
 <style scoped>
-h2 {
-  color: green;
+.contenedorimagen {
+  width: 100%;
+  height: 200px;
+  margin-bottom: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.texto{
-    text-align: center;
-    color:blue;
-    font-size: 40px;
+.contenedorimagen img {
+  width: 400px;
+  height: 200px;
+}
+.titulocomponente {
+  font-size: 30px;
+  padding: 10px;
+  color: black;
 }
 </style>

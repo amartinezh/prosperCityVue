f91f92570adc2f07718288b2a64268f686b685ec
Jose Luis <jj@jj>
2022-08-22 07:52:18 -0500

    se agregaron comentarios, se organizo el componente BitFlow para que funcione con lista de objetos y se cambio el emoticon por un gif

--- src/components/ComponentesMultimedia/MultimediaComentario.vue
+++ src/components/ComponentesMultimedia/MultimediaComentario.vue
@@ -4,11 +4,14 @@
 <br>
 
 <label for="name">
+  <!-- en el input vamos a cacturar nuestro texto en la variable comentar -->
   <input v-model="comentar"  class="form-control" @keyup.enter="agregarcomentario"
   placeholder="presiona enter o de click en comentar" type="text" />
 </label><br><br>
+<!-- aplicamos el evento click para cuando presionemos el boton llamemos a la funcion-->
 <button @click="agregarcomentario" class="btn btn-primary btn-sm">Comentar</button><br>
 <ul class="list-grouplist-group-flush">
+  <!-- recorremos toda nuestra lista de comentarios para mostrar comentario por comentario -->
   <li class="list-group-item" v-for="(c) in comentarios" :key = "c">{{c}} </li>
 </ul>
 
--- src/components/ComponentesMultimedia/MultimediaFotos.vue
+++ src/components/ComponentesMultimedia/MultimediaFotos.vue
@@ -1,37 +1,57 @@
 <template>
-    <h2>Fotografías</h2>
+    <h2 class="text-center display-4 my-4">Fotografías</h2>
+    <!-- creamos el boton que hace llamado al modal -->
+    <a href="#!"  data-bs-toggle="modal" data-bs-target="#agregar_imagen">
+          <button class="btn btn-primary">INGRESAR NUEVA FOTO</button>
+    </a>
+    <!-- creamos las configuraciones del modal -->
+    <div tabindex="-1" aria-labelledby="agregar_imagen" aria-hidden="true"
+     class="modal fade" id="agregar_imagen">
+      <div class="modal-dialog  modal-lg modal-dialog-centered">
+          <div class="modal-content">
+            <!-- header del modal-->
+            <div class="modal-header">
+             <h5 class="display-5">ingresa la imagen nueva</h5>
+            </div>
+            <!-- cuerpo del modal -->
+            <div class="modal-body">
+              <!-- en los inputs esta los v-model que hace el llamado a la variable
+              el cual va a guardar lo que escribamos en el input en la variable-->
+              <span class="label label-default display-5">imagen</span>
+              <input type="text" class="display-5" v-model="imagen"
+              placeholder="ingresa la url de la imagen">
+              <span class="label label-default display-5">descripcion</span>
+              <input type="text" v-model="descripcion"
+               placeholder="ingresa la descripcion" class="display-5">
+            </div>
+            <!-- footer del modal -->
+            <dir class="model-footer">
+              <!-- agregamos el evento @click al button para llamar la funcion
+              que agrega lo que colocamos en los v-model a la lista -->
+              <button class="btn btn-danger close display-5"
+               data-dismiss='modal' @click="agregarImagen">agregar</button>
+            </dir>
+          </div>
+      </div>
+  </div>
     <br>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <div class="imagen-container">
+        <!-- se realizo un ciclo for el cual va a iterar toda la lista
+    mostrando la descripcion y la imagen -->
         <div class="imagen-item imagen-8" v-for="i in imagenes" :key = i>
             <h3 class="subtitle" >{{i.descripcion}}</h3>
             <hr>
             <img v-bind:src= "i.imagen" alt='no dio' v-bind:title= "i.descripcion">
         </div>
     </div>
-    <div class="imagen-container" v-for="i in imagenes" :key = i>
-      <div class="imagen-item imagen-8">
-          <h3 class="subtitle" >prueba</h3>
-          <hr>
-          <a href="#!"  data-bs-toggle="modal" v-bind:data-bs-target=i.modal>
-          <img v-bind:src= i.imagen alt='no dio' title= "prueba">
-        </a>
-      </div>
-          <!-- modal galery -->
-    <div tabindex="-1" v-bind:aria-labelledby=i.modal_id aria-hidden="true"
-     class="modal fade" v-bind:id=i.modal_id>
-      <div class="modal-dialog modal-lg modal-dialog-centered">
-          <div class="modal-content">
-              <img v-bind:src=i.imagen alt="">
-          </div>
-      </div>
-  </div>
-  <!-- find modal galery -->
-  </div>
 </template>
 
 <script>
 export default {
+  // creamos la lista de objetos la cual va a contener la imagen ,
+  // la descripcion y las variables
+  // para almacenar los datos que vamos a enviar a la lista desde los v-model
   data() {
     return {
       imagenes: [{
@@ -59,7 +79,17 @@ export default {
         imagen: 'https://i.postimg.cc/J0CvZB0K/AI-9.jpg', descripcion: 'Metaverso', modal: '#item8', modal_id: 'item8',
       }],
       imagen: '',
+      descripcion: '',
     };
+  },
+  // funcion que va agregar nuestra imagen y descripcion a la lista
+  methods: {
+    agregarImagen() {
+      this.imagenes.push({
+        imagen: this.imagen, descripcion: this.descripcion, modal: '#item8', modal_id: 'item8',
+      });
+      this.imagen = '';
+    },
   },
 };
 </script>

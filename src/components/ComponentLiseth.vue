e4d7751117e4e6d4c48335ff09d9ae07eb7c1db3
Ricardo Montoya <ricardomontoyain@gmail.com>
2022-08-31 19:49:37 -0500

    ultimos cambios

--- src/components/ComponentLiseth.vue
+++ src/components/ComponentLiseth.vue
@@ -1,23 +1,62 @@
 <template>
- <div>
-  <p class="nombrePajaro">Frutero pechinegro</p>
-    <p class="nombreCientifico">
-      Pipreola riefferi
-    </p>
-  </div>
-</template>
+  <div>
+   <br/>
+     <hr/>
+     <br/>
+   <p class="nombrePajaro">Ibis calvo del norte</p>
+     <p class="nombreCientifico">
+      Geronticus eremita
+     </p>
+     <br/>
+     <hr/>
+     <br/>
+     <div class="contenedor"><img src="../assets/geronticus.jpg" alt="Ibis calvo"/></div>
+     <div class="contenedor">
+       <p>
+        El ibis eremita o ibis calvo del norte (Geronticus eremita)
+        es una especie de ave pelecaniforme de la familia
+        Threskiornithidae propia de Europa, Oriente próximo y África.
+        Está en peligro de extinción y solo sobrevivió en estado
+        silvestre en Marruecos; y escasamente en Siria.
+       </p>
+       <p>
+        El ibis eremita es un ave grande y plumaje negro brillante,
+        que mide entre 70–80 cm de largo y tiene una envergadura
+        alar de entre 125–135 cm y suele pesar entre 1,0–1,3 kg.
+        Su plumaje negro presenta iridiscencias verdes, broncíneas y violáceas.
+        Sobre todo, las plumas cobertoras alares superiores tienen un brillo metálico
+        luminoso, rojo cobrizo. Los ibis eremitas se caracterizan por tener la
+        cabeza y el sector de la garganta sin plumas, con una carúncula de color
+        rojo claro, como su largo pico, que mide 11,5 - 14,7 centímetros, y está
+        ligeramente curvado hacia abajo, y también sus largas patas son rojas.
+       </p>
+     </div>
+  <br/><br/>
+  <hr/>
+  <br/>
+   </div>
+ </template>
 <script lang="ts">
 </script>
 <style scoped>
-  .nombrePajaro {
-    color: black;
-    font-weight:bold;
-    font-size: 2vw;
-  }
-  .nombreCientifico{
-    text-align: center;
-    color:rgb(0, 0, 0);
-    font-size: 1vw;
-    font-style: italic;
-  }
+   .nombrePajaro {
+     color: black;
+     font-weight:bold;
+     font-size: 2vw;
+   }
+   .nombreCientifico{
+     text-align: center;
+     color:rgb(0, 0, 0);
+     font-size: 1vw;
+     font-style: italic;
+   }
+   img{
+     width: 300px;
+   }
+   .contenedor{
+     text-align: center;
+     display:inline-block;
+     width: 25%;
+     vertical-align: top;
+   }
 </style>
--- src/components/ComponentRicardo.vue
+++ src/components/ComponentRicardo.vue
@@ -1,9 +1,41 @@
 <template>
  <div>
-  <p class="nombrePajaro">Cacique candela</p>
+  <br/>
+    <hr/>
+    <br/>
+  <p class="nombrePajaro">Afrechero</p>
     <p class="nombreCientifico">
-      Hypopyrrhus pyrohypogaster
+      Zonotrichia capensis
     </p>
+    <br/>
+    <hr/>
+    <br/>
+    <div class="contenedor"><img src="../assets/afrechero.jpg" alt="Afrechero"/></div>
+    <div class="contenedor">
+      <p>
+        El afrechero o copetón es una especie de ave
+        paseriforme neotropical de la familia Emberizidae.
+        Es la única de esta distribución entre los mal llamados
+        "gorriones americanos" del género Zonotrichia.
+      </p>
+      <p>
+        Es similar a la especie alóctona del gorrión doméstico (Passer domesticus),
+        distinguiéndose por poseer un copete con rayas negras y una figura menos rotunda.
+      </p>
+      <p>
+        Mide entre 13 y 15 cm de largo, pesa entre 20 y 25 g.
+        El pico es corto y recto, de unos 15 mm.
+        Muestra la corona y la cara grises, con una banda negra.
+        La garganta es blanca, con un visible collar en la nuca de color canela o castaño rojizo.
+        El vientre y el pecho son pardo claro o blanquecinos,
+        con reflejos más oscuros y los costados grisáceos.
+        El dorso es también pardo, manchado de negro, con las alas y la cola de tono más oscuro.
+        Los juveniles tienen plumaje más uniforme, con jaspeado más oscuro en el pecho.
+      </p>
+    </div>
+    <br/>
+  <hr/>
+  <br/>
   </div>
 </template>
 <script lang="ts">
@@ -19,5 +51,16 @@
     color:rgb(0, 0, 0);
     font-size: 1vw;
     font-style: italic;
+  }
+
+  img{
+    width: 300px;
+  }
+
+  .contenedor{
+    text-align: center;
+    display:inline-block;
+    width: 25%;
+    vertical-align: top;
   }
 </style>
